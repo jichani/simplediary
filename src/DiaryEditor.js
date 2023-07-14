b51045e6b870b6caec5bf45fa2da0f43837c1a55
jichani <jichan7571@naver.com>
2023-07-14 23:43:03 +0900

    데이터 추가하기

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ onCreate }) => {
   const authorInput = useRef();
   const contentInput = useRef();
 
@@ -30,8 +30,13 @@ const DiaryEditor = () => {
       return;
     }
 
-    console.log(state);
+    onCreate(state.author, state.content, state.emotion);
     alert("저장 성공");
+    setState({
+      author: "",
+      content: "",
+      emotion: 1,
+    })
   }
 
   return <div className="DiaryEditor">
