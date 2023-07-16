e371bd5b5fbd253990286efd7e8f0588ee99670c
jichani <jichan7571@naver.com>
2023-07-16 21:04:37 +0900

    api 호출하기

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
 
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "임지찬",
-//     content: "하이1",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "ddd지찬",
-//     content: "하이2",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "임지찬",
-//     content: "하이2",
-//     emotion: 2,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 4,
-//     author: "임지찬22",
-//     content: "하이123",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-// ]
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_data: new Date().getTime(),
+        id: dataId.current++,
+      }
+    })
+
+    setData(initData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
 
   const onCreate = (author, content, emotion) => {
     const created_data = new Date().getTime();
