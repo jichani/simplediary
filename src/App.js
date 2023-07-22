fab5ec330239b09f8ad156bd9b7bf5f05a927c56
jichani <jichan7571@naver.com>
2023-07-22 11:44:29 +0900

    useMemo완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
@@ -46,7 +46,7 @@ function App() {
   }
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다.`)
+    // console.log(`${targetId}가 삭제되었습니다.`)
     const newDiaryList = data.filter((it) => it.id !== targetId);
     // console.log(newDiaryList);
     setData(newDiaryList);
@@ -60,9 +60,26 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(
+    () => {
+      // console.log("일기 분석 시작");
+
+      const goodCount = data.filter((it) => it.emotion >= 3).length;
+      const badCount = data.length - goodCount;
+      const goodRatio = (goodCount / data.length) * 100;
+      return { goodCount, badCount, goodRatio };
+    }, [data.length]
+  );
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div >
   );
